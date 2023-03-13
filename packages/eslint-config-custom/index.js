24a7e0f28e22c11f85107febbaffdcd46ce03233
Michael Pappas <michael.pappas@oohmedia.com.au>
2023-03-13 11:00:11 +1100

    initial commit

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,6 +1,20 @@
 module.exports = {
+  root: true,
   extends: ["next", "turbo", "prettier"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "indent": ["warn", 2, {
+      "SwitchCase": 1
+    }],
+    "react/jsx-key": "warn",
+    "react/display-name": "off",
+    "no-unused-vars": "off", // or "@typescript-eslint/no-unused-vars": "off",
+    "unused-imports/no-unused-imports": "error",
+    "unused-imports/no-unused-vars": [
+      "warn",
+      { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+    ]
   },
+  ignorePatterns: ["index.js", "**/dist/*", "**/migrations/*", "**/*.typegen.ts"],
+  plugins: ["unused-imports"],
 };
